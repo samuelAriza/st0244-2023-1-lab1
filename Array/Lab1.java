9faa13ba0a1cd249434dccd227a1acd7f9be964d
Samuel Ariza <samargo0317@gmail.com>
2023-04-15 12:13:56 -0500

    Fixed errors in Array, print more accurate arithmetic mean

--- Array/Lab1.java
+++ Array/Lab1.java
@@ -1,15 +1,17 @@
+//Class Lab1
 class Lab1{
-    static{
-        System.loadLibrary("arithmeticMean");
-    }
-    private native int getArithmeticMean(int[] arrayNumbers);
+  //Static library is loaded	
+  static{
+    System.loadLibrary("arithmeticMean");
+  }
+   private native double getArithmeticMean(int[] arrayNumbers);
 
-    public static void main(String[] args){
-		int[] arrayOfNumbers = new int[args.length];
-		int len = args.length;
-		for (int i = 0; i < len; i++){
-			arrayOfNumbers[i] = Integer.parseInt(args[i]);
-		}
-        new Lab1().getArithmeticMean(arrayOfNumbers);
+   public static void main(String[] args){
+     int[] arrayOfNumbers = new int[args.length];
+     int len = args.length;
+     for (int i = 0; i < len; i++){
+       arrayOfNumbers[i] = Integer.parseInt(args[i]);
+     }
+     new Lab1().getArithmeticMean(arrayOfNumbers);
 	}
 }
--- Array/Main.cc
+++ Array/Main.cc
@@ -3,14 +3,15 @@
 #include "Lab1.h"
 using namespace std;
 
-JNIEXPORT jint JNICALL Java_Lab1_getArithmeticMean(JNIEnv *env, jobject, jintArray arrayOfNumbers){
+JNIEXPORT jdouble JNICALL Java_Lab1_getArithmeticMean(JNIEnv *env, jobject, jintArray arrayOfNumbers){
     jint length = env->GetArrayLength(arrayOfNumbers);
     jint *array = env->GetIntArrayElements(arrayOfNumbers, 0);
-    int add = 0;
+    double add = 0;
     for (int i = 0; i < length; i++){
-        add = add + array[0];
+        add = add + array[i];
     }
-    int mean = add / length;
-    cout << mean << << "" <<endl;
+    double mean = add / length;
+    cout << mean << endl;
     return 0;
 }
+
